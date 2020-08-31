f9841b803d6e83f9404c2c4343e9267d5899d367
Thomas Hammer <tjhammer845@gmail.com>
2020-08-31 15:31:19 -0400

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  renderTags() {
+    if (this.state.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  // constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+  // Note -- no need to bind this when you use arrow function below --  = () =>
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Shopping Cart</h1>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    const zeroText = <h2>Zero</h2>;
+    return count === 0 ? zeroText : count;
+  }
+}
+
+export default Counter;
