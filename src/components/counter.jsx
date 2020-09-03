7a8d3e724a5c2d14b9cb37b3a209a1768b5ef1e7
Thomas Hammer <tjhammer845@gmail.com>
2020-09-02 23:00:25 -0400

    counter and counters components created

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,20 +2,20 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"],
+    count: this.props.value,
+    // ,tags: ["tag1", "tag2", "tag3"],
   };
 
-  renderTags() {
-    if (this.state.length === 0) return <p>There are no tags!</p>;
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  // renderTags() {
+  //   if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+  //   return (
+  //     <ul>
+  //       {this.state.tags.map((tag) => (
+  //         <li key={tag}>{tag}</li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
 
   // constructor() {
   //     super();
@@ -23,26 +23,25 @@ class Counter extends Component {
   // }
   // Note -- no need to bind this when you use arrow function below --  = () =>
 
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
   render() {
     return (
       <div>
-        <h1>Shopping Cart</h1>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement()}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()}
+        {/* {this.state.tags.length === 0 && "Please create a new tag!"} */}
+        {/* {this.renderTags()} */}
       </div>
     );
   }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
