c2d9daaa5910f85e7d450af438c945c0119aee2e
Thomas Hammer <tjhammer845@gmail.com>
2020-09-14 19:36:43 -0400

    main container made fluid

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,49 +1,49 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  componentDidUpdate(prevProps, prevState) {
-    console.log("prevProps", prevProps);
-    console.log("prevState", prevState);
-    // if (prevProps.counter.value !== this.props.counter.value) { Ajax call to get new data from the server }
-  }
+    componentDidUpdate(prevProps, prevState) {
+        console.log("prevProps", prevProps);
+        console.log("prevState", prevState);
+        // if (prevProps.counter.value !== this.props.counter.value) { Ajax call to get new data from the server }
+    }
 
-  componentWillUnmount() {
-    console.log("Counter - Unmount");
-  }
+    componentWillUnmount() {
+        console.log("Counter - Unmount");
+    }
 
-  render() {
-    console.log("Counter - Rendered");
+    render() {
+        console.log("Counter - Rendered");
 
-    return (
-      <div>
-        <span className={this.getBadgeClasses()}>{this.formatValue()}</span>
-        <button
-          onClick={() => this.props.onIncrement(this.props.counter)}
-          className="btn btn-secondary btn-sm"
-        >
-          Add
+        return (
+            <div>
+                <span className={this.getBadgeClasses()}>{this.formatValue()}</span>
+                <button
+                    onClick={() => this.props.onIncrement(this.props.counter)}
+                    className="btn btn-secondary btn-sm"
+                >
+                    Add
         </button>
-        <button
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-          className="btn btn-danger btn-sm m-2"
-        >
-          Delete
+                <button
+                    onClick={() => this.props.onDelete(this.props.counter.id)}
+                    className="btn btn-danger btn-sm m-2"
+                >
+                    Delete
         </button>
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.props.counter.value === 0 ? "warning" : "primary";
-    return classes;
-  }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
+        return classes;
+    }
 
-  formatValue() {
-    const { value } = this.props.counter;
-    const zeroText = <h2>Zero</h2>;
-    return value === 0 ? zeroText : value;
-  }
+    formatValue() {
+        const { value } = this.props.counter;
+        const zeroText = <h2>Zero</h2>;
+        return value === 0 ? zeroText : value;
+    }
 }
 
 export default Counter;
