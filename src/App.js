e9906980ac5505dc9f0ebe530de815341af90a1e
Thomas Hammer <tjhammer845@gmail.com>
2020-09-15 00:27:53 -0400

    use slice on handleIncrement, title added

--- src/App.js
+++ src/App.js
@@ -4,64 +4,65 @@ import Counters from "./components/counters";
 import "./App.css";
 
 class App extends Component {
-    state = {
-        counters: [
-            { id: 1, value: 0 },
-            { id: 2, value: 0 },
-            { id: 3, value: 0 },
-            { id: 4, value: 0 },
-        ],
-    };
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
 
-    constructor(props) {
-        super(props);
-        // this.state = this.props.something;
-        console.log("App - Constructor");
-    }
+  constructor(props) {
+    super(props);
+    // this.state = this.props.something;
+    console.log("App - Constructor");
+  }
 
-    componentDidMount() {
-        // Example - Ajax call
-        // this.setState({products})
-        console.log("App - Mounted");
-    }
+  componentDidMount() {
+    // Example - Ajax call
+    // this.setState({products})
+    console.log("App - Mounted");
+  }
 
-    handleReset = () => {
-        const counters = this.state.counters.map((c) => {
-            c.value = 0;
-            return c;
-        });
-        this.setState({ counters });
-    };
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
 
-    handleDelete = (counterId) => {
-        const counters = this.state.counters.filter((c) => c.id !== counterId);
-        this.setState({ counters });
-    };
-    handleIncrement = (counter) => {
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index] = { ...counter };
-        counters[index].value++;
-        this.setState({ counters });
-    };
-    render() {
-        console.log("App - Rendered");
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+  handleIncrement = (counter) => {
+    // const counters = [...this.state.counters];
+    const counters = this.state.counters.slice(); // slice can be used to make a copy of the existing array instead of using above (spread operator)
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+  render() {
+    console.log("App - Rendered");
 
-        return (
-            <React.Fragment>
-                <NavBar
-                    totalCounters={this.state.counters.filter((c) => c.value > 0).length}
-                />
-                <main className="container-fluid mt-4">
-                    <Counters
-                        counters={this.state.counters}
-                        onReset={this.handleReset}
-                        onIncrement={this.handleIncrement}
-                        onDelete={this.handleDelete}
-                    />
-                </main>
-            </React.Fragment>
-        );
-    }
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <main className="container-fluid mt-4">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 export default App;
